7fc3fef2c3d8568ff4bb6e8028cb69b9bab38378
Nathanael Schweers <NSchweers@online.de>
2015-03-21 14:13:13 +0100

    stuff

--- code/setup-slime.el
+++ code/setup-slime.el
@@ -2,4 +2,10 @@
     (setq inferior-lisp-program
           "sbcl"))
 
+(require 'ac-slime)
+(add-hook 'slime-mode-hook 'set-up-slime-ac)
+(add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'slime-repl-mode))
+
 (provide 'setup-slime)
