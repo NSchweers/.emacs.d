13911ad54502bd468e20d86c1ac28ea801df567a
Nathanael Schweers <NSchweers@online.de>
2015-05-07 19:12:50 +0200

    Added irc hotkeys
    Added a macro to slightly shorten the definition of a hydra for irc.
    
    Also got rid of a warning.

--- code/setup-keybindings.el
+++ code/setup-keybindings.el
@@ -12,7 +12,9 @@
 ;;                      (global-set-key (kbd "M-x") 'smex)
 ;;                      (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 ;;                      ;; This is your old M-x.
-;;                      (global-set-key (kbd "C-c M-x") 'execute-extended-command))
+;;                      (global-set-key (kbd "C-c M-x")
+;;                      'execute-extended-command))
+(global-set-key (kbd "C-c M-x") 'execute-extended-command)
 
 ;; Scroll the buffer with keystrokes.
 ;; (global-set-key (kbd "C-S-n") 'scroll-up-1)
