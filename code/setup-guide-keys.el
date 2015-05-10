893d784952886ed1ff624bbcdb203cc93df72220
Nathanael Schweers <NSchweers@online.de>
2015-05-10 18:28:09 +0200

    Added a missing config file.

---
+++ code/setup-guide-keys.el
@@ -0,0 +1,12 @@
+;; -*- lexical-binding: t -*-
+
+(require 'guide-key)
+(setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x 5" "C-x v" "C-x 8"
+                                     "C-x n" "C-c C-x"))
+(guide-key-mode 1)
+(setq guide-key/recursive-key-sequence-flag t)
+(setq guide-key/popup-window-position 'bottom)
+(setq guide-key/idle-delay 0.1)
+
+
+(provide 'setup-guide-keys)
