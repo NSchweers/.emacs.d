b906fd233e90a6095fe7b2a22a45d94948758b8e
Nathanael Schweers <NSchweers@online.de>
2015-05-25 22:16:22 +0200

    some minor helm changes

--- code/setup-guide-keys.el
+++ code/setup-guide-keys.el
@@ -2,7 +2,7 @@
 
 (require 'guide-key)
 (setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x 5" "C-x v" "C-x 8"
-                                     "C-x n" "C-c C-x"))
+                                     "C-x n" "C-c C-x" "C-x c"))
 (guide-key-mode 1)
 (setq guide-key/recursive-key-sequence-flag t)
 (setq guide-key/popup-window-position 'bottom)
