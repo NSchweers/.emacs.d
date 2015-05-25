fbb84646bed7f110b517535b83487a279fd0a501
Nathanael Schweers <NSchweers@online.de>
2015-05-25 22:16:22 +0200

    some minor helm changes

--- code/setup-helm.el
+++ code/setup-helm.el
@@ -3,6 +3,8 @@
 (require 'helm-config)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-M-y") 'helm-show-kill-ring)
+(setq helm-man-or-woman-function 'woman)
+(setq helm-man-or-woman-function 'Man-getpage-in-background)
 (helm-mode 1)
 
 (provide 'setup-helm)
