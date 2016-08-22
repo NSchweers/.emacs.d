2f9b71c2d5d3779fd6f9a95190178d07d32c1755
Nathanael Schweers <NSchweers@online.de>
2016-08-22 21:40:17 +0200

    Added RSS to gnus.

--- gnus.el
+++ gnus.el
@@ -1,79 +1,64 @@
-(setq user-mail-address "NSchweers@online.de")
+(require 'setup-bbdb)
+
+(setq gnus-home-directory (expand-file-name "code" user-emacs-directory))
+(setq user-mail-address "NSchweers@mailbox.org")
 (setq user-full-name "Nathanael Schweers")
 
-;(setq nntp-send-authinfo (expand-file-name ".authfile.gpg" (getenv "HOME")))
-
-(setq gnus-select-method 
-      '(nnimap "1&1"
-               (nnimap-address "imap.1und1.de")
+(setq gnus-select-method
+      '(nnimap "mailbox.org"
+               (nnimap-address "imap.mailbox.org")
                (nnimap-authenticator login)
-               (nnimap-server-port 993)
-               (nnimap-stream ssl))
+               (nnimap-server-port 143)
+               (nnimap-stream starttls))
       gnus-secondary-select-methods
-      '((nnimap "mailbox.org"
-                (nnimap-address "imap.mailbox.org")
+      '((nnimap "1&1"
+                (nnimap-address "imap.1und1.de")
                 (nnimap-authenticator login)
-                (nnimap-server-port 143)
-                (nnimap-stream starttls))))
-
-;; (nnimap-authinfo-file
-;;  (expand-file-name ".authinfo" (getenv "HOME")))
+                (nnimap-server-port 993)
+                (nnimap-stream ssl))))
 
 (setq message-send-mail-function 'smtpmail-send-it
-      smtpmail-starttls-credentials '(("smtp.1und1.de" 587 nil nil))
-      smtpmail-auth-credentials '(("smtp.1und1.de" 587
-				   "NSchweers@online.de" nil))
-      smtpmail-default-smtp-server "smtp.1und1.de"
-      smtpmail-smtp-server "smtp.1und1.de"
-      smtpmail-smtp-service 587
-      ;gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]"
-      )
+      smtpmail-starttls-credentials '(("smtp.mailbox.org" 587 nil nil))
+      smtpmail-auth-credentials '(("smtp.mailbox.org" 587
+                                   "NSchweers@mailbox.org" nil))
+      smtpmail-default-smtp-server "smtp.mailbox.org"
+      smtpmail-smtp-server "smtp.mailbox.org"
+      smtpmail-smtp-service 587)
 
 (setq nnimap-inbox "INBOX")
-(setq nnimap-split-methods
-      `(("INBOX/PUG" 
-         "\\(?:^Subject:.*\\[PUG\\]\\)\\|\\(?:^To:.*talk@pug.org\\)\\|\\(?:Reply-To:.*talk@pug.org\\)")
-        ("INBOX/Geocaching" "^From:.*@geocaching.com")
-        ("INBOX/NEO" 
-         ,(concat "\\(?:^Subject:.*\\[Neo\\]\\)\\|\\(?:^To:.*diskussion@neo-layout.org\\)\\|"
-                 "\\(?:Reply-To:.*diskussion@neo-layout.org\\)"))
-        ("INBOX/FH" "\\(?:^From:.*@.*fra-uas.de\\)\\|\\(?:^Reply-To:.*@.*fra-uas.de\\)")
-        ("INBOX/klingemann"
-         "^To:.*klingemann-geschaedigte-ev@lists.informatiktreffpunkt.de")
-        ("INBOX/emacs-help"
-         "\\(?:^To:.*help-gnu-emacs@gnu.org\\)\\|\\(?:^Cc:.*help-gnu-emacs@gnu.org\\)")
-        ("INBOX/emacs-devel"
-         "\\(?:^To:.*emacs-devel@gnu.org\\)\\|\\(?:^Cc:.*emacs-devel@gnu.org\\)")
-        ("INBOX/FRITZBox"
-         "\\(?:^From:.*\"FRITZ!Box\" <wgschweers@online.de>\\)")
-        ("INBOX/Geocaching"
-         "\\(?:From:.*@geocaching.com\\)")
-        ("INBOX/Amazon"
-         "\\(?:From:.*@amazon.de\\)")
-        ("INBOX" "")))
 
-;; (setq gnus-select-method '(nmimap "1&1"
-;;                                   (nmimap-address "imap.1und1.de")
-;;                                   (nmimap-server-port 993)
-;;                                   (nmimap-stream ssl)))
+(setf nnrss-directory (expand-file-name "News/rss" user-emacs-directory))
 
-;; (setq mail-sources '((pop :server "pop.provider.org"
-;;                           :user "you"
-;;                           :password "secret")))
+(require 'mm-url)
 
+(defadvice mm-url-insert (after DE-convert-atom-to-rss () )
+  "Converts atom to RSS by calling xsltproc."
+  (when (re-search-forward "xmlns=\"http://www.w3.org/.*/Atom\"" 
+			   nil t)
+    (goto-char (point-min))
+    (message "Converting Atom to RSS... ")
+    (call-process-region (point-min) (point-max) 
+			 "xsltproc" 
+			 t t nil 
+			 (expand-file-name "site-lisp/atom2rss.xsl"
+                                           user-emacs-directory) "-")
+    (goto-char (point-min))
+    (message "Converting Atom to RSS... done")))
 
+(ad-activate 'mm-url-insert)
 
-;; (setq gnus-select-method
-;;       '(nnimap "1&1"
-;; 	       (nnimap-address "imap.1und1.de")
-;; 	       (nnimap-server-port 993)
-;; 	       (nnimap-stream ssl)))
+(add-hook 'message-mode-hook (lambda ()
+                               (turn-on-orgtbl)
+                               (define-key message-mode-map (kbd "C-c C-w")
+                                 'kill-region)
+                               (define-key message-mode-map (kbd "C-M-o")
+                                 'schweers/split-quoted-paragraph)))
 
-;; (setq message-send-mail-function 'smtpmail-send-it
-;;       smtpmail-starttls-credentials '(("smtp.1und1.de" 587 nil nil))
-;;       smtpmail-auth-credentials '(("smtp.1und1.de" 587
-;; 				   "NSchweers@online.de" nil))
-;;       smtpmail-default-smtp-server "smtp.1und1.de"
-;;       smtpmail-smtp-server "smtp.1und1.de"
-;;       smtpmail-smtp-service 587
-;;       gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
+(add-hook 'message-mode-hook (lambda ()
+                               (turn-on-orgtbl)
+                               (define-key message-mode-map (kbd "C-M-o")
+                                 'schweers/split-quoted-paragraph)))
+(bbdb-initialize)
+
+(add-hook 'gnus-startup-hook 'bbdb-insinuate-gnus)
+(add-hook 'gnus-startup-hook 'bbdb-insinuate-message)
