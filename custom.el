5b35d80ad3ccbd51b1e21084707e56e0bd86329d
Nathanael Schweers <NSchweers@online.de>
2016-10-13 19:05:36 +0200

    Does it work?

--- custom.el
+++ custom.el
@@ -47,7 +47,7 @@
      "\\tolerance=1000")))
  '(package-selected-packages
    (quote
-    (shell-command undo-tree ace-jump-mode helm jabber expand-region ac-slime browse-kill-ring ace-link multiple-cursors bbdb auctex elpy magit auto-complete lispy guide-key org paren-face crux helm-projectile projectile cider racer cargo clojure-mode w3m json-mode f geiser diminish s dash-functional hydra use-package dash)))
+    (markdown-mode shell-command undo-tree ace-jump-mode helm jabber expand-region ac-slime browse-kill-ring ace-link multiple-cursors bbdb auctex elpy magit auto-complete lispy guide-key org paren-face crux helm-projectile projectile cider racer cargo clojure-mode w3m json-mode f geiser diminish s dash-functional hydra use-package dash)))
  '(rcirc-default-nick "schweers")
  '(rcirc-default-user-name "schweers")
  '(rcirc-fill-column (quote frame-width))
