a0c8521fa0e7017bcd3631e05dc3f7ebc24e3817
Nathanael Schweers <NSchweers@mailbox.org>
2016-11-23 20:27:04 +0100

    Done!
    I now have a working literate configuration for Emacs!

--- init.el
+++ init.el
@@ -3,5 +3,11 @@
 
 ;; (package-initialize)
 
-(org-babel-load-file
- (expand-file-name "literate-init.org" user-emacs-directory))
+;; (org-babel-load-file
+;;  (expand-file-name "literate-init.org" user-emacs-directory))
+
+(condition-case nil
+    (load (expand-file-name "bootstrap.el" user-emacs-directory))
+  (file-error (org-babel-load-file
+               (expand-file-name "literate-init.org" user-emacs-directory))))
+(load-literate-init)
