3fbe58d456da6e295e6df00782c5a288324f3a4d
Nathanael Schweers <Nathanael.Schweers@outdooractive.com>
2017-03-23 15:39:41 +0100

    Minor stuff

--- custom.el
+++ custom.el
@@ -3,6 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(TeX-view-program-selection
+   (quote
+    (((output-dvi has-no-display-manager)
+      "dvi2tty")
+     ((output-dvi style-pstricks)
+      "dvips and gv")
+     (output-dvi "xdvi")
+     (output-pdf "Okular")
+     (output-html "xdg-open"))))
  '(ac-use-quick-help t)
  '(auth-source-save-behavior nil)
  '(auth-sources (quote ("~/.emacs_data/authinfo.gpg")))
@@ -59,7 +68,7 @@
      "\\tolerance=1000")))
  '(package-selected-packages
    (quote
-    (notmuch fuel tracwiki-mode helm-gtags bash-completion edit-server go-scratch go-eldoc go-mode async tramp-term bbdb-vcard evil-org evil-lispy evil-magit rust-mode htmlize csharp-mode elfeed evil markdown-mode shell-command undo-tree ace-jump-mode helm jabber expand-region ac-slime browse-kill-ring ace-link multiple-cursors bbdb auctex elpy magit auto-complete lispy guide-key org paren-face crux helm-projectile projectile cider racer cargo clojure-mode w3m json-mode f geiser diminish s dash-functional hydra use-package dash lua-mode)))
+    (go-scratch go-eldoc go-mode fuel notmuch bash-completion edit-server helm-gtags tracwiki-mode async tramp-term bbdb-vcard evil-org evil-lispy evil-magit rust-mode htmlize csharp-mode elfeed evil markdown-mode shell-command undo-tree ace-jump-mode helm jabber expand-region ac-slime browse-kill-ring ace-link multiple-cursors bbdb auctex elpy magit auto-complete lispy guide-key org paren-face crux helm-projectile projectile cider racer cargo clojure-mode w3m json-mode f geiser diminish s dash-functional hydra use-package dash lua-mode)))
  '(rcirc-default-nick "schweers")
  '(rcirc-default-user-name "schweers")
  '(rcirc-fill-column (quote frame-width))
@@ -71,7 +80,9 @@
      ("irc.freenode.net" :channels
       ("#neo" "#informatik-ag")
       nil nil))))
- '(send-mail-function (quote smtpmail-send-it)))
+ '(send-mail-function (quote smtpmail-send-it))
+ '(smtpmail-smtp-server "smtp.1und1.de" t)
+ '(smtpmail-smtp-service 25 t))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
