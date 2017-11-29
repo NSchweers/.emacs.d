f6378c84f376066ed317fab64af418ad558a9995
Nathanael Schweers <NSchweers@mailbox.org>
2017-11-29 22:00:07 +0100

    Trying out ivy & counsel instead of helm.
    Also removed old evil code which I’m not going to be using anytime soon.

--- custom.el
+++ custom.el
@@ -92,7 +92,7 @@
      "\\tolerance=1000")))
  '(package-selected-packages
    (quote
-    (smartscan org-plus-contrib edit-server dumb-jump flycheck-rust cargo rust-mode protobuf-mode cmake-mode tramp-term lua-mode flycheck-haskell haskell-mode elfeed w3m paren-face helm-projectile projectile cider clojure-mode undo-tree helm-swoop expand-region js2-mode browse-kill-ring ace-link multiple-cursors notmuch bbdb auctex markdown-mode graphviz-dot-mode htmlize elpy magit helm-company slime-company company lispy which-key shell-command bash-completion json-mode f dash-functional dash async paradox use-package)))
+    (flx ivy-hydra counsel smartscan org-plus-contrib edit-server dumb-jump flycheck-rust cargo rust-mode protobuf-mode cmake-mode tramp-term lua-mode flycheck-haskell haskell-mode elfeed w3m paren-face helm-projectile projectile cider clojure-mode undo-tree helm-swoop expand-region js2-mode browse-kill-ring ace-link multiple-cursors notmuch bbdb auctex markdown-mode graphviz-dot-mode htmlize elpy magit helm-company slime-company company lispy which-key shell-command bash-completion json-mode f dash-functional dash async paradox use-package)))
  '(paradox-github-token t)
  '(rcirc-default-nick "schweers")
  '(rcirc-default-user-name "schweers")
